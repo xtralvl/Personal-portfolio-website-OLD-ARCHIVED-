8afedd587d650e2248eab8a44404df1214c10ce2
Roland Szucs <rolandszucs.private@gmail.com>
2025-04-24 16:40:35 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// Coding Time Counter //
+
+const codingTime = document.getElementById("coding-time");
+const startTime = new Date("2025-01-03T12:00:00");
+
+function updateCounter() {
+const now = new Date();
+const elapsedTime = now - startTime; // Elapsed time in milliseconds //
+const minutes = Math.floor(elapsedTime / 60000);
+const seconds = Math.floor((elapsedTime % 60000) / 1000);
+const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+const secondText = seconds === 1 ? 'second' : 'seconds'; // Pluralize if necessary
+
+codingTime.textContent = `I've spent ${minutes} minutes and ${formattedSeconds} ${secondText} with coding so far.`;
+};
+
+setInterval(updateCounter, 1000);
