d78a9b721259c2bfcdb8ce18a8878978e63e1eb6
Roland Szucs <rolandszucs.private@gmail.com>
2025-04-26 16:50:58 +0200

    finished dark mode

--- script.js
+++ script.js
@@ -16,3 +16,32 @@ codingTime.textContent = `I started to code approximately ${minutes} minutes and
 
 setInterval(updateCounter, 1000);
 
+
+// Dark-Light Mode Switch //
+
+const lightIcon = document.getElementById('light');
+const darkIcon = document.getElementById('dark');
+const icons = document.getElementById('modebuttons');
+
+// Light Mode Styles //
+lightIcon.addEventListener('click', () => {
+    document.body.classList.remove('dark-mode');
+    darkIcon.style.opacity = '0.5';
+    lightIcon.style.opacity = '1';
+    lightIcon.style.transform = 'scale(1.1)';
+    darkIcon.style.transform = 'scale(0.8)';
+    icons.style.border = '2px solid black';
+
+});
+
+// Dark Mode Styles //
+darkIcon.addEventListener('click', () => {
+    document.body.classList.add('dark-mode');
+    darkIcon.style.opacity = '1';
+    lightIcon.style.opacity = '0.5';
+    darkIcon.style.color = 'black';
+    lightIcon.style.color = 'black';
+    lightIcon.style.transform = 'scale(0.8)';
+    darkIcon.style.transform = 'scale(1.1)';
+    icons.style.border = '2px solid #87cefa';
+});
