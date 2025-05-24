f4cfd4193e8527306d110583af73750594739bd4
Roland Szucs <rolandszucs.private@gmail.com>
2025-05-24 21:59:18 +0200

    deleted form added learning section

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// Coding Time Counter //
-
-/* const codingTime = document.getElementById("coding-time");
-const startTime = new Date("2025-01-03T12:00:00");
-
-function updateCounter() {
-const now = new Date();
-const elapsedTime = now - startTime; // Elapsed time in milliseconds //
-const days = Math.floor(elapsedTime / 86400000); // days //
-const minutes = Math.floor(elapsedTime / 60000); // minutes //
-const seconds = Math.floor((elapsedTime % 60000) / 1000); // seconds //
-const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
-const secondText = seconds === 1 ? 'second' : 'seconds'; // Pluralize if necessary
-
-codingTime.textContent = `I started to code approximately ${days} days,  ${minutes} minutes and ${formattedSeconds} ${secondText} ago.`;
-};
-
-setInterval(updateCounter, 1000); */
-
-
 // Dark-Light Mode Switch //
 
 const lightIcon = document.getElementById('light');
